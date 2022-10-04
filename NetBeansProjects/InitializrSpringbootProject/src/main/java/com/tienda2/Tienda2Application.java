7accbaf4972be6c68d17796a695642eda0ac8181
Samuelalvaradoo <Samuelalvaradoo>
2022-10-03 20:03:01 -0600

    semana 3 tarea de pagina web

---
+++ NetBeansProjects/InitializrSpringbootProject/src/main/java/com/tienda2/Tienda2Application.java
@@ -0,0 +1,13 @@
+package com.tienda2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tienda2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tienda2Application.class, args);
+	}
+
+}
---
+++ NetBeansProjects/InitializrSpringbootProject/src/test/java/com/tienda2/Tienda2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tienda2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
